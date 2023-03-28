4bc5c8cecd5a9fee40219e6cbd866232302eb1cb
Gergo-Sinka <sinkag88@gmail.com>
2023-03-28 13:32:28 +0200

    JS Finished

---
+++ Cinema-App/db.js
@@ -0,0 +1,28 @@
+const movieList = {
+    currency: "HUF",
+    movies: [
+        {
+            title: "Braveheart",
+            price: 80
+        },
+        {
+            title: "Shrek",
+            price: 70
+        },
+        {
+            title: "Gladiator",
+            price: 100
+        },
+        {
+            title: "Dune",
+            price: 120
+        },
+    ]
+};
+const seatsDetails = {
+    rows: 6,
+    columns: 8,
+    occupied: [11, 17, 28, 38, 39]
+};
+
+export { movieList, seatsDetails };
